33e460fcc8621107dce174f1b08076db382bb6f9
vc2010 <86232932+vc2010@users.noreply.github.com>
2021-07-05 19:50:16 +0530

    Assignment 2 CRUD operation Static and Dynamic

---
+++ assignment2/SelectDynamic.java
@@ -0,0 +1,57 @@
+package com.te.jdbc.assignment2;
+
+import java.util.Properties;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.sql.*;
+
+public class SelectDynamic {
+
+	public static void selectDynamicQuery() {
+		Connection connection=null;
+		PreparedStatement prepare=null;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the ID of the Student whose details you want to view: ");
+		int id=sc.nextInt();
+		try {
+			FileInputStream file= new FileInputStream("directory.properties");
+			Properties properties= new Properties();
+			properties.load(file);
+			String driver=properties.getProperty("driver");
+			String dburl=properties.getProperty("dburl");
+			String user=properties.getProperty("user");
+			String password=properties.getProperty("password");
+			String query=properties.getProperty("querySelectDynamic");
+			Class.forName(driver); 
+			
+			connection = DriverManager.getConnection(dburl,user,password);
+			
+			prepare=connection.prepareStatement(query);
+			prepare.setInt(1, id);
+			
+			ResultSet resultSet=prepare.executeQuery();
+			
+			while (resultSet.next()) {
+				System.out.println("ID: " + resultSet.getInt(1));
+				System.out.println("Name: " + resultSet.getString(2));
+				System.out.println("Standard: " + resultSet.getInt(3));
+				System.out.println("Marks: " + resultSet.getDouble(4));
+				System.out.println("Address: " + resultSet.getString(5));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (connection != null)
+					connection.close();
+				
+				if (prepare!=null)
+					prepare.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ assignment2/StudentMain.java
@@ -0,0 +1,92 @@
+package com.te.jdbc.assignment2;
+
+import java.util.Scanner;
+
+public class StudentMain {
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		char opt;
+		do {
+		System.out.println("------------------------------------------");
+		System.out.println("        Welcome To Student Database");
+		System.out.println("------------------------------------------");
+		System.out.println();
+		System.out.println("What do you want to perform ");
+		System.out.println("1. CRUD Static");
+		System.out.println("2. CRUD Dynamic");
+		System.out.println("3. Exit");
+		System.out.println();
+		System.out.println("Enter your choice:");
+		int ch = scanner.nextInt();
+		
+		switch (ch) {
+		case 1:
+			System.out.println("What do you want to perform ");
+			System.out.println("1. Insert New Student Data");
+			System.out.println("2. Update data of an existing student");
+			System.out.println("3. Delete data of an existing student");
+			System.out.println("4. View all the student's data present in the database");
+			System.out.println("Enter your choice:");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				Insert.insertStatic();
+				break;
+			case 2:
+				System.out.println("What do you want to update:");
+				System.out.println("n:Name\ts:Standard\tm:Marks\td:Date of Birth");
+				String option=scanner.next();
+				Update.updateStatic(option);
+				break;
+			case 3:
+				Delete.deleteStatic();
+				break;
+			case 4:
+				Select.selectStatic();
+				break;
+			default:
+				break;
+			}
+			break;
+		case 2:
+			System.out.println("What do you want to perform ");
+			System.out.println("1. Insert New Student Data");
+			System.out.println("2. Update data of an existing student");
+			System.out.println("3. Delete data of an existing student");
+			System.out.println("4. View a student's data present in the database");
+			System.out.println("Enter your choice:");
+			int choice2 = scanner.nextInt();
+			switch (choice2) {
+			case 1:
+				InsertDynamic.insertDynamicQuery();
+				break;
+			case 2:System.out.println("What do you want to update:");
+				System.out.println("n:Name\ts:Standard\tm:Marks\td:Date of Birth");
+				String option=scanner.next();
+				UpdateDynamic.updateDyanmicQuery(option);
+				break;
+			case 3:
+				DeleteDynamic.deleteDynamicQuery();
+				break;
+			case 4:
+				SelectDynamic.selectDynamicQuery();
+				break;
+			default:
+				break;
+			}
+			break;
+			
+		case 3:System.exit(0);
+			break;
+		default:System.out.println("Invalid choice");
+			break;
+		}
+		System.out.println("Do you want to continue?");
+		System.out.println("Enter 'y' for yes or 'n' for no: ");
+		opt = scanner.next().charAt(0);
+	} while (opt != 'n');
+	}
+
+}
